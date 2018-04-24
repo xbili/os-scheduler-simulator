139c29ff1be800e8116235b4c730b973a2e0a03d
Bili Xu <xubili93@gmail.com>
2018-04-24 15:26:24 +0800

    Refactor test case for FCFS

--- simulator/tests.py
+++ simulator/tests.py
@@ -8,18 +8,30 @@ class FCFSTest(unittest.TestCase):
         self.scheduler = FCFS()
 
     def test_basic_scheduling(self):
+        """
+        Test case for the basic example provided in the assignment
+        specification.
+        """
         processes = create_processes(
             (0, 0, 4),
             (1, 2, 3))
+        expected_schedule = [
+            (0, 0),
+            (4, 1)]
+        expected_avg_waiting_time = 1.0
+        expected = {
+            'schedule': expected_schedule,
+            'avg_waiting_time': expected_avg_waiting_time,
+        }
+
+        self.assert_process_schedule(processes, expected)
+
+    def assert_process_schedule(self, processes, expected):
         schedule = self.scheduler.schedule(processes)
         avg_waiting_time = self.scheduler.avg_waiting_time
 
-        expected = [
-            (0, 0),
-            (4, 1)]
-
-        self.assertEqual(schedule, expected)
-        self.assertEqual(avg_waiting_time, 1.0)
+        self.assertEqual(schedule, expected['schedule'])
+        self.assertEqual(avg_waiting_time, expected['avg_waiting_time'])
 
     def tearDown(self):
         self.scheduler.reset()
