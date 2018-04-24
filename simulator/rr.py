ee8c49f3bce2e3b7368538e8abd1eb80b5a71cf5
Bili Xu <xubili93@gmail.com>
2018-04-24 16:09:22 +0800

    Initial implementation of round robin scheduling

--- simulator/rr.py
+++ simulator/rr.py
@@ -1,11 +1,42 @@
+from collections import deque
+
 from simulator.scheduler import Scheduler
 
 class RoundRobin(Scheduler):
     """Constant time quantum Round Robin scheduler."""
 
     def __init__(self, time_quantum):
-        self.q = time_quantum
+        self.time_q = time_quantum
+
+        # We make use of a deque to keep track of the ready queue
+        self.q = deque()
 
     def schedule(self, processes):
         super(RoundRobin, self).schedule(processes)
-        # TODO: Implementation
+
+        # Sort the tasks in reverse order so that we can pop tasks off
+        ordered = sorted(processes, key=lambda x: -x.arrive_time)
+
+        # Keep running round robin until either we run out of processes
+        res = []
+        while self.q or ordered:
+            # Check if there are any more tasks that should come into the
+            # ready queue.
+            while ordered[-1].arrive_time <= self.current_time:
+                self.q += [ordered.pop()]
+
+            if self.q:
+                # Take the next element in the ready queue
+                nxt = self.q.popleft()
+
+                res += [(self.current_time, nxt.id)]
+
+                # Decrement the burst time
+                nxt.burst_time -= self.time_q
+
+                # Add it back into the queue
+                self.q += [nxt]
+
+            self.current_time += self.time_q
+
+        return res
