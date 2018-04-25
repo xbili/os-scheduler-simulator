de1c3ba494337b6e84a50d5ac6446baba59d9f04
Bili Xu <xubili93@gmail.com>
2018-04-25 14:40:01 +0800

    Use prediction for next CPU burst rather than actual burst

--- simulator/sjf.py
+++ simulator/sjf.py
@@ -18,21 +18,33 @@ class SJF(Scheduler):
         """
         super(SJF, self).schedule(processes)
 
+        tau = {process.id: 0 for process in processes}
         ordered, res = deque(processes), []
+        prev_burst = 0
         while ordered or not self.pq.is_empty():
             elapsed = 1
 
             # Push all arrived processes into the ready queue
             while ordered and ordered[0].arrive_time <= self.current_time:
                 nxt = ordered.popleft()
-                self.pq.add(nxt, priority=nxt.burst_time)
+
+                # Predict the next CPU burst for this process
+                prediction = self.predict_next_burst(tau[nxt.id], nxt.burst_time)
+                tau[nxt.id] = prediction
+
+                self.pq.add(nxt, priority=prediction)
 
             if not self.pq.is_empty():
                 active = self.pq.pop()
                 res += [(self.current_time, active.id)]
                 elapsed = active.burst_time
+                curr_burst = active.burst_time
                 self.waiting_time += (self.current_time - active.arrive_time)
 
             self.current_time += elapsed
 
         return res
+
+    def predict_next_burst(self, current_prediction, current_burst_time):
+        return self.alpha * current_burst_time +\
+            (1-self.alpha) * current_prediction
