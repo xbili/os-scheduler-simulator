5838c766b5de789b0cc6bffb1bdc70348ebd0339
Bili Xu <xubili93@gmail.com>
2018-04-26 16:37:36 +0800

    Switch SRTF scheduler to use the new Scheduler interface

--- simulator/schedulers/srtf.py
+++ simulator/schedulers/srtf.py
@@ -14,58 +14,61 @@ class SRTF(Scheduler):
         super(SRTF, self).__init__()
 
         # Ready queue
-        self.pq = PriorityQueue()
-
-        # Active job
-        self.active = None
+        self.q = PriorityQueue()
 
     def schedule(self, processes):
         super(SRTF, self).schedule(processes)
 
-        ordered, res = deque(processes), []
-        while ordered or not self.pq.is_empty():
-            # Add tasks that arrives into the ready queue
-            while ordered and ordered[0].arrive_time == self.current_time:
-                nxt = ordered.popleft()
-                self.pq.add(nxt, priority=nxt.burst_time)
-
-            # Handle case where there is no current active task
-            if not self.active and not self.pq.is_empty():
-                # Set a new job in the ready queue to be active
-                self.active = self.pq.pop()
-                res += [(self.current_time, self.active.id)]
-            elif not self.active:
-                # No jobs currently, but still have other jobs queued up for
-                # the simulation
-                self.current_time += 1
-                continue
-
-            # Handle cases for replacing jobs. Either:
-            # 1. They have completed execution, or
-            # 2. They are being preempted.
-            if self.active.burst_time == 0:
-                # Load next job
-                if not self.pq.is_empty():
-                    self.active = self.pq.pop()
-                    res += [(self.current_time, self.active.id)]
-                else:
-                    self.active = None
-                    self.current_time += 1
-                    continue
-            elif not self.pq.is_empty() and self.active.burst_time > self.pq.peek().burst_time:
-                # PREEMPT
-                new = self.pq.pop()
-                self.pq.add(self.active, priority=self.active.burst_time)
-                self.active = new
-                res += [(self.current_time, new.id)]
-
-            # Burst time should never be negative
-            assert self.active.burst_time >= 0
-
-            self.active.burst_time -= 1
-            self.current_time += 1
-
-            # Every job in the PQ is waiting
-            self.waiting_time += len(self.pq)
+        self.ordered, res = deque(processes), []
+        while self.q or self.ordered or self.active:
+            self.enqueue_new_jobs()
+            if self.timer_interrupt():
+                process = self.perform_schedule()
+                if process:
+                    res += [(self.current_time, process.id)]
+            self.step()
 
         return res
+
+    def perform_schedule(self):
+        """
+        We perform scheduling in either of the two scenarions:
+        1. A task completes its execution
+        2. A shorter task has arrived in the ready queue.
+        """
+        # No running tasks or task finished execution
+        if not self.active or self.active.burst_time == 0:
+            self.active = self.q.pop() if self.q else None
+            return self.active
+
+        # Shorter task arrived
+        if self.q.peek().burst_time < self.active.burst_time:
+            nxt = self.q.pop()
+            self.q.add(self.active, priority=self.active.burst_time)
+            self.active = nxt
+            return self.active
+
+    def enqueue_new_jobs(self):
+        """
+        (OVERRIDE) - Scheduler.enqueue_new_jobs
+
+        We need to override this to make use of our PriorityQueue API instead.
+        """
+        while self.ordered and self.ordered[0].arrive_time == self.current_time:
+            nxt = self.ordered.popleft()
+            self.q.add(nxt, priority=0)
+
+    def timer_interrupt(self):
+        """
+        (OVERRIDE) - Scheduler.timer_interrupt
+
+        We need to set a timer interrupt when a task of lower burst time comes
+        in to the ready queue as well.
+        """
+        default = super(SRTF, self).timer_interrupt()
+        if self.q and self.active:
+            shorter = self.q.peek().burst_time < self.active.burst_time
+        else:
+            shorter = False
+
+        return default or shorter
